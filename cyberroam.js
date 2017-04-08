451d0edcbd43e71517a6c5a3e84bd0152f01b195
as1733 <as1733@gmail.com>
2017-04-08 23:04:17 +0530

    Update cyberroam.js

--- cyberroam.js
+++ cyberroam.js
@@ -1,6 +1,6 @@
 /**
  * Created by as1733 on 21-03-2017.
  */
-document.getElementsByClassName("textbox")[0].value="BE1043914";
-document.getElementsByClassName("textbox")[1].value="aditya";
+document.getElementsByClassName("textbox")[0].value="YOur rooll no";
+document.getElementsByClassName("textbox")[1].value="your password";
 document.getElementById("logincaption").click();
